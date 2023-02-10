83ca8e1b2a5ce9d2d2e10cd19834c0fa3c914d7f
Alex Kopnin <alsinuse@gmail.com>
2023-02-10 18:39:02 -0500

    Добавлены функции редиректа и преобразования спец.символов htmlspecialchars()

--- vendor/freedom/libs/functions.php
+++ vendor/freedom/libs/functions.php
@@ -8,3 +8,27 @@
         dump($data, $die, $type);
     }
 
+/**
+ * Функция редитекта
+ * @param $url страница на которую надо отредиректить, если не передана, возврат на предыдущую
+ * @return void
+ */
+    function redirect($url = false){
+        if($url){
+            $redirect = $url;
+        }else{
+            // возвращаем на предыдущую страницу если существует, либо на главную
+            $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/';
+        }
+        header("Location: $redirect");
+        exit;
+    }
+
+/** Преобразование (обработка) специальных символов
+ * @param $str
+ * @return string
+ */
+    function h($str){
+        return htmlspecialchars($str, ENT_QUOTES);
+    }
+
