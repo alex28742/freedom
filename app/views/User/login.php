dab72b582a3605097c705743cf6ac8337164741e
Alex Kopnin <alsinuse@gmail.com>
2023-02-10 18:30:42 -0500

    Добавлены формы регистрации и авторизации

--- app/views/User/login.php
+++ app/views/User/login.php
@@ -1,1 +1,28 @@
-<?php echo __FILE__;
+<h2>Авторизация</h2>
+
+<div class="row">
+    <div class="col-md-4">
+        <? if(isset($_SESSION['success'])):?>
+            <div class="alert alert-success">
+                <?=$_SESSION['success']; unset($_SESSION['success']); ?>
+
+            </div>
+        <? endif; ?>
+        <? if(isset($_SESSION['errors'])):?>
+            <div class="alert alert-warning">
+                <?=$_SESSION['errors']; unset($_SESSION['errors']); ?>
+            </div>
+        <? endif; ?>
+        <form method="post" action="/user/login">
+            <div class="mb-3">
+                <label for="login" class="form-label">Login</label>
+                <input type="text" name="login" class="form-control" id="login" value="<?=$login;?>">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Вход</button>
+        </form>
+    </div>
+</div>
--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -1,24 +1,36 @@
-
+<br>
 <div class="container">
     <div class="row">
-        <div class="form-wrapper">
-            <div class="col-md-3">
-                <form method="get" action="/user/signup">
-                    <div class="mb-3">
-                        <label for="name">Имя</label>
-                        <input type="text" class="form-control" id="name">
+        <div class="col-md-3">
+            <div class="form-wrapper">
+                <form method="POST" action="/user/signup">
+                    <? if(isset($_SESSION['errors'])): ?>
+                    <div class="alert alert-danger">
+                        <? echo $_SESSION['errors'];  unset($_SESSION['errors']); ?>
                     </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Адрес электронной почты</label>
-                        <input type="text" class="form-control" id="email" aria-describedby="email">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Пароль</label>
-                        <input type="password" class="form-control" id="password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Зарегистрировать</button>
+                    <? endif; ?>
+                    <? if(isset($_SESSION['success'])): ?>
+                        <div class="alert alert-success">
+                            <? echo $_SESSION['success'];  unset($_SESSION['success']); ?>
+                        </div>
+                    <? endif; ?>
+                    <div class="errors"></div>
+                    <input class="form-control" type="text" name="login" placeholder="login" value="<? if(isset($data['login'])):?><?=$data['login'];?><?endif;?>">
+                    <br>
+
+                    <input class="form-control" type="text" name="name" placeholder="name" value="<? if(isset($data['name'])):?><?=$data['name'];?><?endif;?>">
+                    <br>
+
+                    <input class="form-control" type="text" name="email" placeholder="e-mail" value="<? if(isset($data['email'])):?><?=$data['email'];?><?endif;?>">
+                    <br>
+
+                    <input class="form-control" type="password" name="password" placeholder="password">
+                    <br>
+                    <input class="btn btn-primary" type="submit" value="send">
                 </form>
             </div>
         </div>
+
     </div>
 </div>
+
